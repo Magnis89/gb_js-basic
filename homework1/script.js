8d9aaf4cc3c6b34b3242a4b05579fea3ca57eebd
Valery <magnis1989@ya.ru>
2021-07-13 10:34:31 +0300

    hw-1

---
+++ homework1/script.js
@@ -0,0 +1,25 @@
+// задание 1
+
+/* let degCelsius = prompt('Введите температуру в градусах по Цельсию')
+let degFahrenheit = (9 / 5) * degCelsius + 32
+
+alert('Температура в градусах по Фаренгейту равна ' + degFahrenheit) */
+
+
+// задание 2
+
+/* let naMe = 'Василий'
+let admin = naMe
+
+console.log(admin) */
+
+
+// задание 3
+
+let a = 1000
+let b = '108'
+
+console.log(a + b) // 1000108
+
+// async - скрипт будет выполнятся не дожидаясь загрузки html страницы
+// defer - скрипт будет выполнятся после загрузки html страницы
